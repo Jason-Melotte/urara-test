855da12f554b298e82b8a9bb4f9a6db1e76ddc5d
Jason-Melotte <94680824+Jason-Melotte@users.noreply.github.com>
2023-04-09 07:31:43 +0100

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,15 +3,15 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  title: 'Jason-SvelteKit-Urara Alpha',
+  subtitle: 'My first play with SvelteKit',
   lang: 'en-US',
   description: 'Powered by SvelteKit/Urara',
   author: {
     avatar: '/assets/maskable@512.png',
-    name: 'John Doe',
+    name: 'Jason',
     status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    bio: 'Its Me'
   },
   themeColor: '#3D4451'
 }
